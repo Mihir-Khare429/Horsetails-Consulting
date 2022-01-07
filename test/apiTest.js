feab5c23efb1feea9e841da88e63772be5cc8f86
Mihir Khare <mkhare429@gmail.com>
2022-01-07 10:52:08 +0530

    Added lock required file for CI

--- test/apiTest.js
+++ test/apiTest.js
@@ -1,16 +1,18 @@
-const chai = require('chai'); 
+/* eslint-disable no-undef */
+const chai = require('chai');
 const chaiHttp = require('chai-http');
+
 chai.use(chaiHttp);
 const server = require('../index');
 
 describe('Test', () => {
-    it('it should check the status of server',(done) => {
-        chai.request(server)
-        .get('/')
-        .end((err, res) => {
+  it('it should check the status of server', (done) => {
+    chai.request(server)
+      .get('/')
+      .end((err, res) => {
         (res).should.have.status(200);
         (res.body).should.be.a('object');
         done();
-        })
-    })
-})
+      });
+  });
+});
